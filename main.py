67dc75a54fa9c680c9005ab93c8a76d6299ee53e
Fabian Leuthold <fabian.leuthold@ost.ch>
2021-10-24 12:44:52 +0200

    implementation simple model complete

--- main.py
+++ main.py
@@ -17,134 +17,73 @@ def solve_OrTools(dima):
     :return:  solution X, model, status
     '''
 
+    if dima.ndim != 2 or dima.shape[0] != dima.shape[1]:
+        raise ValueError("Invalid dima dimensions detected. Square matrix expected.")
+
+    # determine number of nodes
+    num_nodes = dima.shape[0]
+    all_nodes = range(0, num_nodes)
+    all_but_first_nodes = range(1, num_nodes)
+
     # Create the model.
     solver_name = 'GUROBI_MIP'
-    log.info('instantiating solver ' + solver_name)
+    log.info('Instantiating solver ' + solver_name)
     model = pywraplp.Solver.CreateSolver(solver_name)
     model.EnableOutput()
 
-    # log.info('Defining MIP model... ')
-    # # generating decision variables X_ij
-    # log.info('Creating ' + str(len(all_services) * len(all_services)) + ' boolean X_ij variables... ')
-    # X = {}
-    # for i in all_services:
-    #     for j in all_services:
-    #         X[(i, j)] = model.BoolVar('X_i%ij%i' % (i, j))
-    #
-    # # generating help variables LF_ij (for Leerfahrten)
-    # log.info('Creating ' + str(len(all_services) * len(all_services)) + ' boolean LF_ij variables... ')
-    # LF = {}
-    # for i in all_services:
-    #     for j in all_services:
-    #         LF[(i, j)] = model.BoolVar('LF_i%ij%i' % (i, j))
-    #
-    # # generating help variables u_i (for subtour elimination)
-    # log.info('Creating ' + str(len(all_services)) + ' boolean u_i variables... ')
-    # u = {}
-    # for i in all_services:
-    #     u[i] = model.IntVar(0, mpar.size, 'u_i%i' % i)
-    #
-    # # constraint 1: accessibility and empty runs
-    # log.info('Creating ' + str(len(all_services) * len(all_services)) + ' Constraint 1... ')
-    # for i in all_services:
-    #     for j in all_services:
-    #         log.debug('Constraint 1, (i,j)=(' + str(i) + ', ' + str(j))
-    #         model.Add(X[(i, j)] * (1 - s[(i, j)]) <= LF[(i, j)])
-    #         # model.Add(X[(i, j)] <= s[(i, j)])
-    #
-    # # constraint 3.1: fake start-service has no predecessor
-    # log.info('Creating ' + str(len(fake_start_services)) + ' Constraint 3.1... ')
-    # for j in fake_start_services:
-    #     model.Add(sum(X[(i, j)] for i in all_services) == 0)
-    #
-    # # constraint 3.2: fake end-service has no successor
-    # log.info('Creating ' + str(len(fake_end_services)) + ' Constraint 3.2... ')
-    # for i in fake_end_services:
-    #     model.Add(sum(X[(i, j)] for j in all_services) == 0)
-    #
-    # # constraint 4.1: all non-fake end services must be served exactly once
-    # log.info('Creating ' + str(len(all_services)) + ' Constraint 4.1... ')
-    # for i in all_services_non_fake_end:
-    #     model.Add(sum(X[(i, j)] for j in all_services) == 1)
-    #
-    # # constraint 4.2: all non-fake start service must be served exactly once
-    # log.info('Creating ' + str(len(fake_end_services)) + ' Constraint 4.2... ')
-    # for j in all_services_non_fake_start:
-    #     model.Add(sum(X[(i, j)] for i in all_services) == 1)
-    #
-    # # constraint 4.3: fake-services may not be connected
-    # log.info('Creating ' + str(len(fake_end_services)) + ' Constraint 4.3... ')
-    # model.Add(X[(fake_start_services[0], fake_end_services[0])] == 0)
-    #
-    # # constraint 6: the plan must have exactly the specified duration
-    # log.info('Creating 1 Constraint 6... ')
-    # model.Add(sum(X[(i, j)] * p[(i, j)] for i in all_services for j in all_services) == mpar.tc_num_weeks * 7 + 1)
-    #
-    # # constraint 7.1: subtour elimination constraints (Miller-Tucker-Zemlin) part 1
-    # log.info('Creating 1 Constraint 7.1... ')
-    # model.Add(u[0] == 1)
-    #
-    # # constraint 7.2: subtour elimination constraints (Miller-Tucker-Zemlin) part 2
-    # log.info('Creating ' + str(len(all_services_non_zero)) + ' Constraint 7.2... ')
-    # for i in all_services_non_zero:
-    #     model.Add(2 <= u[i])
-    #     # TODO: check < or <= ? should be <, I suppose
-    #     model.Add(u[i] <= mpar.size)
-    #
-    # # constraint 7.3: subtour elimination constraints (Miller-Tucker-Zemlin) part 3
-    # log.info('Creating ' + str(len(all_services_non_zero)) + ' Constraint 7.2... ')
-    # for i in all_services_non_zero:
-    #     for j in all_services_non_zero:
-    #         model.Add(u[i] - u[j] + 1 <= (mpar.size - 1)*(1 - X[(i, j)]))
-    #
-    # # Minimierung der Anzahl geänderte Dienste und Anzahl Dienstfahrten
-    # model.Minimize(
-    #     weight_alpha_1 * sum(LF[(i, j)] for i in all_services for j in all_services))
-    #
-    # log.info('Solving MIP model... ')
-    # status = model.Solve()
-    #
-    return X, model, status
+    log.info('Defining MIP model... ')
+    # generating decision variables X_ij
+    log.info('Creating ' + str(num_nodes * num_nodes) + ' boolean x_ij variables... ')
+    x = {}
+    for i in all_nodes:
+        for j in all_nodes:
+            x[(i, j)] = model.BoolVar('x_i%ij%i' % (i, j))
 
-def get_services_dict(X, LF, num_services, fake_start_rowid, fake_end_rowid):
-    '''
-    create a map from service-row-id to the 0-based index of service for all services (containing fake start-/end-svcs)
+    log.info('Creating ' + str(num_nodes) + ' boolean u_i variables... ')
+    u = {}
+    for i in all_nodes:
+        u[i] = model.IntVar(0, num_nodes, 'u_i%i' % i)
 
-    :param X: the decision variable X_ij
-    :param LF: the decision variable LF_ij
-    :param num_services: the number of services
-    :param fake_start_rowid: the rowid of the only fake start service
-    :param fake_end_rowid: the rowid of the only fake end service
-    :return: a map from service-row-id to the 0-based index of service for all services (containing fake start-/end-svcs)
-             as well as a list containing the start service-row-id of all (i,j) tupels requiring a Leerfahrt
-    '''
-    # first, create a map from each row-id to successor row-id
-    successor_map = {}
-    lf_list = []
-    all_services = range(0, num_services)
-    for i in all_services:
-        for j in all_services:
-            # this following check should be executed conditionally if X_ij = 1, but for reasons of better
-            # error-detection / transparency we check this here independently!
-            if LF[(i, j)].solution_value() == 1:
-                lf_list.append(i)
-                # log.info('LF ' + str(i) + '/' + str(j))
-            if X[(i, j)].solution_value() == 1:
-                successor_map[i] = j
-                # log.info('Service ' + str(i) + '/' + str(j))
-                break
-    # now create the result map
-    next_row_id = fake_start_rowid
-    row_id = 0
-    sequential_rowid_dict = {}
-    lf_list_trans = []
-    while next_row_id in successor_map:
-        sequential_rowid_dict[next_row_id] = row_id
-        if next_row_id in lf_list:
-            lf_list_trans.append(row_id)
-        next_row_id = successor_map[next_row_id]
-        row_id = row_id + 1
-    return sequential_rowid_dict, lf_list_trans
+    # constraint 1: leave every point exactly once
+    log.info('Creating ' + str(num_nodes) + ' Constraint 1... ')
+    for i in all_nodes:
+        model.Add(sum(x[(i, j)] for j in all_nodes) == 1)
+
+    # constraint 2: reach every point from exactly one other point
+    log.info('Creating ' + str(num_nodes) + ' Constraint 2... ')
+    for j in all_nodes:
+        model.Add(sum(x[(i, j)] for i in all_nodes) == 1)
+
+    # constraint 3.1: subtour elimination constraints (Miller-Tucker-Zemlin) part 1
+    log.info('Creating 1 Constraint 3.1... ')
+    model.Add(u[0] == 1)
+
+    # constraint 3.2: subtour elimination constraints (Miller-Tucker-Zemlin) part 2
+    log.info('Creating ' + str(len(all_but_first_nodes)) + ' Constraint 3.2... ')
+    for i in all_but_first_nodes:
+        model.Add(2 <= u[i])
+        model.Add(u[i] <= num_nodes)
+
+    # constraint 3.3: subtour elimination constraints (Miller-Tucker-Zemlin) part 3
+    log.info('Creating ' + str(len(all_but_first_nodes)) + ' Constraint 3.2... ')
+    for i in all_but_first_nodes:
+        for j in all_but_first_nodes:
+            model.Add(u[i] - u[j] + 1 <= (num_nodes - 1) * (1 - x[(i, j)]))
+
+    # Minimize the total distance
+    model.Minimize(sum(x[(i, j)] * dima[(i, j)] for i in all_nodes for j in all_nodes))
+
+    log.info('Solving MIP model... ')
+    status = model.Solve()
+
+    return u, model, status
+
+
+def print_solution(u):
+    num_nodes = len(u)
+    all_nodes = range(0, num_nodes)
+    for i in range(0, all_nodes):
+        log.info('u(' + str(i) + ')=' + str(u[i].solution_value))
 
 def main():
     # configure logger for info level
@@ -155,10 +94,10 @@ def main():
         stream=sys.stdout)
 
     # load tsp instance
-    tsp_problem = 'qa194.tsp'
+    tsp_problem = 'dj38.tsp'
 
     log.info("Reading TSP problem Instance " + tsp_problem)
-    tsp = pd.read_csv('./TSP_Instances/' + tsp_problem, sep=' ', skiprows=7, dtype = float,
+    tsp = pd.read_csv('./TSP_Instances/' + tsp_problem, sep=' ', skiprows=10, dtype = float,
                       names=['nodeId', 'lat', 'lng'], skipfooter=1, engine='python')
     tsp = tsp.sort_values(by='nodeId', inplace=False)
 
@@ -166,13 +105,14 @@ def main():
     dima = sp.distance_matrix(A, A)
 
     # now solve problem
-    X, model, status = solve_OrTools(dima)
+    u, model, status = solve_OrTools(dima)
 
     # check problem response
     if status == pywraplp.Solver.OPTIMAL:
         log.info('Solution:')
         log.info('optimal solution found.')
         log.info('Objective value =' + str(model.Objective().Value()))
+        print_solution(u)
     elif status == pywraplp.Solver.INFEASIBLE:
         log.info('The problem is infeasible.')
     else:
